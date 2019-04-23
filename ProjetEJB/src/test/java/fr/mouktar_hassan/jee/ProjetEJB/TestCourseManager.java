a44501b1da419997abb43dfcf1fb8d494b95911d
Utilisateur <Utilisateur@10.130.239.173>
2019-04-24 01:38:32 +0200

    Ajoute des certains test unitaire!
    Change-Id: I6aeb8eeac46ffac276548b10c25dd5f19ddb60d4

--- ProjetEJB/src/test/java/fr/mouktar_hassan/jee/ProjetEJB/TestCourseManager.java
+++ ProjetEJB/src/test/java/fr/mouktar_hassan/jee/ProjetEJB/TestCourseManager.java
@@ -2,13 +2,18 @@ package fr.mouktar_hassan.jee.ProjetEJB;
 
 import static org.junit.Assert.assertNotNull;
 
+import java.util.List;
+
 import javax.ejb.EJB;
 import javax.ejb.embeddable.EJBContainer;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
+import monapp.Course;
 import monapp.CourseManager;
 
 public class TestCourseManager {
@@ -29,6 +34,47 @@ public class TestCourseManager {
     @Test
     public void testFindCourses() {
         assertNotNull(cm.findCourses());
+    }
+    
+    @Test
+    public void testSaveCourses() {
+         Course course=new Course();
+         course.setName("Java EE");
+         course.setDescription("Programmation distribué");
+         course.setLevel("6");
+         course.setHours(6);
+         cm.saveCourse(course);
+         
+         assertNotNull(cm);
+    }
+    
+    @Ignore
+    @Test
+    public void testFindCourse() {
+    	Course course=new Course();
+        course.setName("Java EE");
+        course.setDescription("Programmation distribué");
+        course.setLevel("6");
+        course.setHours(6);
+        cm.saveCourse(course);
+        
+        List<Course> courses=(List<Course>) cm.findCourse((long)1);
+        Assert.assertEquals(1,courses.size());        
+    	
+    }
+    
+    @Test
+    public void testDeleteCourse() {
+    	Course course=new Course();
+        course.setName("Cloud computing");
+        course.setDescription("Virtualisation par les services AWS");
+        course.setLevel("2");
+        course.setHours(10);
+        cm.saveCourse(course);
+        long id=course.getId();
+        cm.deleteCourse(course);
+        Assert.assertNull(cm.findCourse(id));
+        
     }
 
 }
